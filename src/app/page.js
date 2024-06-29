d7bd998e514fcf953ed99ff09667bfc36e9b126a
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-06-29 23:04:45 +0100

    new commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,22 @@
-import Image from "next/image";
+import { Inter, Actor, Roboto } from "next/font/google";
+import "./globals.css";
 
-export default function Home() {
+
+const inter = Inter({ subsets: ["latin"] });
+const actor = Actor({ subsets: ["latin"], weight: ["400"] });
+const roboto = Roboto({ subsets: ["latin"], weight: ["300", "400"] });
+
+export const metadata = {
+  title: "MentMeet",
+  description: "MentMeet",
+};
+
+export default function RootLayout({ children }) {
   return (
-    <div>
-      
-   </div>
+    <html lang="en">
+      <body className={inter.className}>
+        {children}
+      </body>
+    </html>
   );
 }
