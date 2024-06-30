deac512f16b13f72cae3b38606877363beb704f5
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-06-30 23:15:33 +0100

    new commit

---
+++ src/components/Menu.jsx
@@ -0,0 +1,103 @@
+"use client";
+
+import React, { useState } from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { CgMenu } from "react-icons/cg";
+import { MdOutlineClose } from "react-icons/md";
+import { IoMdArrowDropdown, IoMdArrowDropup } from "react-icons/io";
+
+const links = [
+  { id: 1, title: "Homepage", url: "/" },
+  { id: 2, title: "Investing", url: "/investing" },
+  { id: 4, title: "Earning", url: "/earning" },
+  { id: 4, title: "Learning", url: "/learning" },
+];
+
+const Menu = () => {
+  const [open, setOpen] = useState(false);
+  const [showInvestDropdown, setShowInvestDropdown] = useState(false);
+  const [arrowIcon, setArrowIcon] = useState(<IoMdArrowDropdown size={24} />);
+
+  const handleInvestHover = () => {
+    setShowInvestDropdown(true);
+  };
+
+  const handleInvestLeave = () => {
+    setShowInvestDropdown(false);
+  };
+
+  const handleInvestClick = () => {
+    setShowInvestDropdown(!showInvestDropdown);
+    setArrowIcon(
+      showInvestDropdown ? (
+        <IoMdArrowDropdown size={24} />
+      ) : (
+        <IoMdArrowDropup size={24} />
+      )
+    );
+  };
+
+  const pathname = usePathname();
+
+  return (
+    <div>
+      {!open ? (
+        <CgMenu size="2rem" onClick={() => setOpen(true)} />
+      ) : (
+        <MdOutlineClose size="2rem" onClick={() => setOpen(false)} />
+      )}
+      {open && (
+        <div className="bg-white text-black absolute left-0 top-24 w-full h-[calc(100vh-6rem)] flex flex-col gap-8 pl-6 items-start justify-center text-3xl font-serif2 z-10">
+          {links.map((item) => (
+            <React.Fragment key={item.id}>
+              {item.title === "Investing" ? (
+                <div
+                  className="relative"
+                  onMouseEnter={handleInvestHover}
+                  onMouseLeave={handleInvestLeave}
+                >
+                  <p
+                    className="flex items-center cursor-pointer"
+                    onClick={handleInvestClick}
+                  >
+                    <span>{item.title}</span>
+                    {arrowIcon}
+                  </p>
+                  {showInvestDropdown && (
+                    <div className="absolute flex flex-col top-full left-0 bg-white text-black py-2 px-4 ">
+                      <Link href="/investing">
+                        <p
+                          className={`text-base font-serif ${
+                            pathname === "/investing" ? "text-[#79C947]" : ""
+                          }`}
+                        >
+                          Invest
+                        </p>
+                        <p className="text-sm text-[#767676] font-mono">
+                          Investment Account
+                        </p>
+                      </Link>
+                    </div>
+                  )}
+                </div>
+              ) : (
+                <Link href={item.url} onClick={() => setOpen(false)}>
+                  <p
+                    className={`text-2xl font-serif ${
+                      pathname === item.url ? "text-[#79C947]" : ""
+                    }`}
+                  >
+                    {item.title}
+                  </p>
+                </Link>
+              )}
+            </React.Fragment>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Menu;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import Link from "next/link";
+import Menu from "./Menu";
+import Image from "next/image";
+
+const Navbar = () => {
+  return (
+    <div className="h-20 w-full text-black flex flex-row px-3 md:flex-row items-center justify-between md:h-24 lg:px-20 xl:px-40">
+      {/* LEFT SIDE */}
+      <div className="flex md:hidden">
+        {/* LOGO */}
+        <Link href="/">
+          <Image
+            src="/logo.png"
+            alt="logo"
+            width={135}
+            height={195}
+            className="object-center object-cover mt-1 pl-0"
+          />
+        </Link>
+      </div>
+      <div className="hidden md:block">
+        <Image
+          src="/logo.png"
+          alt="logo"
+          width={135}
+          height={195}
+          className="object-center object-cover mt-1 pl-0"
+        />
+      </div>
+      <div className="hidden md:flex gap-4 flex-1 items-center justify-center">
+        {/* Links */}
+        <Link href="/">Investing</Link>
+        <Link href="/banking">Banking</Link>
+        <Link href="/earning">Earning</Link>
+        <Link href="/learning">Learning</Link>
+        <Link href="/learning"></Link>
+      </div>
+      {/* Right Side - Visible on Small Screens */}
+      <div className="md:hidden flex flex-row justify-center items-center space-x-3 pt-5 pb-5">
+        {" "}
+        {/* Modified container */}
+        <div className="md:hidden">
+          <Menu />
+        </div>
+      </div>
+      {/* Right Side - Hidden on Small Screens */}
+      <div className="hidden md:flex items-center justify-center space-x-3">
+        <Link href="/login">Login</Link>
+        <button className="bg-[#79C947] text-white px-3 py-3 rounded-full hidden md:block">
+          Get Started
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
