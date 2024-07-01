a46db3ad83e5bd798264c48e3617882027e450d3
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-07-01 23:38:49 +0100

    new commit

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -6,17 +6,12 @@ import { CgMenu } from "react-icons/cg";
 import { MdOutlineClose } from "react-icons/md";
 import { IoMdArrowDropdown, IoMdArrowDropup } from "react-icons/io";
 
-const links = [
-  { id: 1, title: "Homepage", url: "/" },
-
-];
+const links = [{ id: 1, title: "Homepage", url: "/" }];
 
 const Menu = () => {
   const [open, setOpen] = useState(false);
   const [showInvestDropdown, setShowInvestDropdown] = useState(false);
   const [arrowIcon, setArrowIcon] = useState(<IoMdArrowDropdown size={24} />);
-
- 
 
   return (
     <div>
@@ -28,10 +23,7 @@ const Menu = () => {
       {open && (
         <div className="bg-white text-black absolute left-0 top-24 w-full h-[calc(100vh-6rem)] flex flex-col gap-8 pl-6 items-start justify-center text-3xl font-serif2 z-10">
           {links.map((item) => (
-            <React.Fragment key={item.id}>
-             
-            
-            </React.Fragment>
+            <React.Fragment key={item.id}></React.Fragment>
           ))}
         </div>
       )}
