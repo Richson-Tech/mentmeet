2c316961d91baca8e61eefdb9314cd5d76c7667a
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-07-04 00:01:18 +0100

    new commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter, Actor, Roboto } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 const actor = Actor({ subsets: ["latin"], weight: ["400"] });
@@ -13,7 +14,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar/>
+        {children}
+      </body>
     </html>
   );
 }
