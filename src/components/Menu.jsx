e76255124824e71168f6aaa97686e9b8c6bc610b
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-07-03 23:51:31 +0100

    new commit

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -24,7 +24,9 @@ const Menu = () => {
       {open && (
         <div className="bg-white text-black absolute left-0 top-24 w-full h-[calc(100vh-6rem)] flex flex-col gap-8 pl-6 items-start justify-center text-3xl font-serif2 z-10">
           {links.map((item) => (
-            <React.Fragment key={item.id}></React.Fragment>
+           <Link key={item.id} href={item.url} onClick={() => setOpen(false)}>
+           {item.title}
+         </Link>
           ))}
         </div>
       )}
