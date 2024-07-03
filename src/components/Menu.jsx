933ed99527f660186f09f1f84cabbaf8a114c6d1
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-07-03 23:52:05 +0100

    new commits

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -6,7 +6,7 @@ import { CgMenu } from "react-icons/cg";
 import { MdOutlineClose } from "react-icons/md";
 
 const links = [
-  { id: 1, title: "Homepage", url: "/" },
+  { id: 1, title: "MentMeet", url: "/" },
   { id: 2, title: "Explore", url: "/explore" },
   { id: 3, title: "meet", url: "/meet" },
 ];
@@ -24,9 +24,9 @@ const Menu = () => {
       {open && (
         <div className="bg-white text-black absolute left-0 top-24 w-full h-[calc(100vh-6rem)] flex flex-col gap-8 pl-6 items-start justify-center text-3xl font-serif2 z-10">
           {links.map((item) => (
-           <Link key={item.id} href={item.url} onClick={() => setOpen(false)}>
-           {item.title}
-         </Link>
+            <Link key={item.id} href={item.url} onClick={() => setOpen(false)}>
+              {item.title}
+            </Link>
           ))}
         </div>
       )}
