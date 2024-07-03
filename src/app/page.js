4711fc37c913b87f113473b34c32129e1432145a
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-07-04 00:00:18 +0100

    new commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,9 @@
-import { Inter, Actor, Roboto } from "next/font/google";
-import "./globals.css";
+import First from "@/components/First";
 
-const inter = Inter({ subsets: ["latin"] });
-const actor = Actor({ subsets: ["latin"], weight: ["400"] });
-const roboto = Roboto({ subsets: ["latin"], weight: ["300", "400"] });
-
-export const metadata = {
-  title: "MentMeet",
-  description: "MentMeet",
-};
-
-export default function RootLayout({ children }) {
+export default function Home() {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <main>
+      <First/>
+    </main>
   );
 }
